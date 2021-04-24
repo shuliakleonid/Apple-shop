5a82313801b9b142b9d081a434ff8d80721806a7
Shuliak Leonid <zeleny777@gmail.com>
2021-04-24 06:41:59 +0300

    add admin page with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
       {path: 'product/:id', component: ProductPageComponent},
       {path: 'cart', component: CartPageComponent}
     ]
+  },
+  {
+    // path: 'admin', loadChildren: './admin/admin.module#AdmineModule'
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   }
 ];
 
